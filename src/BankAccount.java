76ed9ed48c3c7ce860e595d8c7ab384e588cc3f6
unknown <dnal03@msn.com>
2023-05-11 22:14:16 -0400

    Class, Object & Constructor

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class BankAccount {
+public class BankAccount {
+
+    //Attributes that can only be used in this class (private)
+    private String acctHolderName;
+    private double acctBalance;
+
+    //Constructor that takes the attributes as parameters to initialize
+    public BankAccount(String acctHolderName, double acctBalance) {
+        this.acctHolderName = acctHolderName;
+        this.acctBalance = acctBalance;
+    }
+
+    //Different methods created
+    public void deposit(double depositAmnt) {
+        acctBalance += depositAmnt;
+    }
+
+    public void withdrawal (double amount) {
+//        if (amount > this.acctBalance) {
+//            System.out.println("Insufficient funds");
+//        } else {
+//            this.acctBalance = amount;
+//        }
+        acctBalance -= amount;
+    }
+
+    public void printDetails() {
+        System.out.println("My balance is " + this.acctBalance + "\n");
+    }
+
+    public String toString() {
+        return acctHolderName + "'s account balance: " + acctBalance;
+    }
+
 }
--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Product {
+
+public class Product {
+    public static void main(String[] args) {
+
+        Product gas = new Product(4.0, 10, "Gallon of gas");
+        gas.printProduct();
+        gas.totalCost();
+
+        //Attributes that can only be used in this class (private)
+        private double costOfProduct;
+        private int quantity;
+        private String name;
+
+    }
+
+    public Product(double costOfProduct, int quantity, String name) {
+        this.costOfProduct = costOfProduct;
+        this.quantity = quantity;
+        this.name = name;
+    }
+
+    public void totalCost() {
+        double totalPurchase = costOfProduct * quantity;
+        System.out.println("Total costs is");
+    }
+
+    public void printProduct() {
+        System.out.println(name + " costs " + costOfProduct + " and " + quantity + " units were purchased");
+    }
 }
+
